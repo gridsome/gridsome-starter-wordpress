16b22f46ace14b9fdaef5231b2f315bed9ac0c59
Peter Thompson <pete@chronicdigital.com>
2019-02-16 09:54:20 -0500

    Updated Post template to display categories for each post. Updated Category template to display a list of posts for the given category

--- src/templates/WordPressCategory.vue
+++ src/templates/WordPressCategory.vue
@@ -1,17 +1,62 @@
 <template>
   <Layout>
-    ...
+    <h1>Category: 
+      <span v-html="$page.category.title" />
+    </h1>
+    <ul class="post-list">
+      <li v-for="post in postsInCategory" :key="post._id">
+        <h2 v-html="post.node.title"/>
+        <p v-html="post.node.excerpt"/>
+        <router-link :to="post.node.path">Read more</router-link>
+      </li>
+    </ul>
   </Layout>
 </template>
 
 <page-query>
-query Category ($path: String!) {
-  wordPressCategory (path: $path) {
+query Category ($path: String, $page: Int) {
+  category: wordPressCategory (path: $path) {
     title
+    slug
+  }
+  posts: allWordPressPost (page: $page) {
+    edges {
+      node {
+        _id
+        title
+        excerpt
+        path
+        categories {
+          slug
+        }
+      }
+    }
   }
 }
 </page-query>
 
 <script>
-export default {}
+export default {
+  computed: {
+    postsInCategory: function () {
+      let cat = this.$page.category.slug;
+      let arr = this.$page.posts.edges;
+      let newArr = arr.filter(function(item){
+        return item.node.categories[0].slug === cat;
+      })
+      return newArr;
+    }
+  }
+}
 </script>
+
+<style>
+  .post-list {
+    list-style: none;
+    padding-left: 0;
+
+  }
+  .post-list li {
+    padding: 1em 0;
+  }
+</style>
--- src/templates/WordPressPost.vue
+++ src/templates/WordPressPost.vue
@@ -1,6 +1,14 @@
 <template>
   <Layout>
     <h1 v-html="$page.wordPressPost.title"/>
+    <span>Category: </span>
+    <ul>
+      <li v-for="category in $page.wordPressPost.categories" :key="category.id" >
+        <router-link :to="category.path">
+          <span v-html="category.slug" />
+        </router-link>
+      </li>
+    </ul>
     <div v-html="$page.wordPressPost.content"/>
   </Layout>
 </template>
@@ -10,6 +18,11 @@ query Post ($path: String!) {
   wordPressPost (path: $path) {
     title
     content
+    categories {
+      id
+      slug
+      path
+    }
   }
 }
 </page-query>
