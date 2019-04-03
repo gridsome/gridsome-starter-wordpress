7e51d13fe32e9378ab1d6d16cf8b9db81af14776
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-04-03 16:32:46 +0100

    Update src/templates/WordPressPost.vue
    Change `g-image` to `img`
    
    Co-Authored-By: thetre97 <travis97@outlook.com>

--- src/templates/WordPressPost.vue
+++ src/templates/WordPressPost.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <h1 v-html="$page.wordPressPost.title"/>
-    <g-image
+    <img
       v-if="$page.wordPressPost.featuredMedia"
       :src="$page.wordPressPost.featuredMedia.sourceUrl"
       :width="$page.wordPressPost.featuredMedia.mediaDetails.width"
