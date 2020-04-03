f9dfdfc3f6faddf8431f343ba8079411b5f18db6
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2020-04-03 19:03:43 +0200

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,9 +3,9 @@ module.exports = {
   siteDescription: 'A WordPress starter for Gridsome',
 
   templates: {
-    WordPressCategory: '/category/:slug', // adds route for "category" post type (Optional)
-    WordPressPost: '/:year/:month/:day/:slug', //adds route for "post" post type (Optional)
-    WordPressPostTag: '/tag/:slug' // adds route for "post_tag" post type (Optional)
+    WordPressCategory: '/category/:slug', // adds a route for the "category" post type (Optional)
+    WordPressPost: '/:year/:month/:day/:slug', // adds a route for the "post" post type (Optional)
+    WordPressPostTag: '/tag/:slug' // adds a route for the "post_tag" post type (Optional)
   },
 
   plugins: [
